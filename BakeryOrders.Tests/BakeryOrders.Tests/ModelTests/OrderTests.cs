f5a6b0d15250d4d29d5cfb40229f93343c0e0d79
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-15 10:04:38 -0700

    add and pass Order GetTitle test

--- BakeryOrders.Tests/BakeryOrders.Tests/ModelTests/OrderTests.cs
+++ BakeryOrders.Tests/BakeryOrders.Tests/ModelTests/OrderTests.cs
@@ -19,5 +19,19 @@ namespace BakeryOrders.Tests
       Order newOrder = new Order("test Order");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_String() 
+    {
+      // Arrange
+      string Title = "Suzie's Cafe-07/15/2022";
+      Order newOrder = new Order(Title);
+
+      // Act
+      string result = newOrder.Title;
+
+      // Assert (test)
+      Assert.AreEqual(Title, result);
+    }
   }
 }
