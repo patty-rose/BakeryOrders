a22daac84de93e35ef6c87f424442db1570494dc
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-15 09:56:15 -0700

    pass Vendor FindVendor test

--- BakeryOrders/Models/Vendor.cs
+++ BakeryOrders/Models/Vendor.cs
@@ -4,10 +4,12 @@ namespace BakeryOrders.Models
 {
   public class Vendor
   {
+    // auto implemented properties
     public string Name { get; set; }
     public int Id { get; }
     private static List<Vendor> _instances = new List<Vendor> {};
 
+    //constructor
     public Vendor(string vendorName)
     {
       Name = vendorName;
@@ -15,13 +17,20 @@ namespace BakeryOrders.Models
       Id = _instances.Count;
     }
 
+    //class methods
     public static void ClearAll()
     {
       _instances.Clear();
     }
+
     public static List<Vendor> GetAll()
     {
       return _instances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
