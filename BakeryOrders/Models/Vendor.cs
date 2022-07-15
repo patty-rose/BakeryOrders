32895006e52d0dd6891da2043d2ab4da6cf6b56e
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-15 09:44:12 -0700

    pass Vendor GetId test

--- BakeryOrders/Models/Vendor.cs
+++ BakeryOrders/Models/Vendor.cs
@@ -5,10 +5,14 @@ namespace BakeryOrders.Models
   public class Vendor
   {
     public string Name { get; set; }
+    public int Id { get; }
+    private static List<Vendor> _inistances
 
     public Vendor(string vendorName)
     {
       Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
     }
   }
 }
