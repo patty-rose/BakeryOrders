238df236ec1c7d85f6e7ca5cc1c7ab7ec65be050
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-15 09:41:23 -0700

    add and pass Vendor GetName test

--- BakeryOrders.Tests/BakeryOrders.Tests/ModelTests/VendorTests.cs
+++ BakeryOrders.Tests/BakeryOrders.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,19 @@ namespace BakeryOrders.Tests
       Vendor newVendor = new Vendor("test Vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
   }
 }
