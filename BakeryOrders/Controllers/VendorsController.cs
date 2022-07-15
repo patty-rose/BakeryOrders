0506fa058eb701d7b6e678149c23f2c336b35def
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-15 14:00:35 -0700

    add basic controllers scripts

---
+++ BakeryOrders/Controllers/VendorsController.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using BakeryOrders.Models;
+
+namespace BakeryOrders.Controllers
+{
+  public class VendorsController : Controller
+  {
+  }
+}
