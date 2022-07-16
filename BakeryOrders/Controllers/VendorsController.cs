f97410cee31e38630779743fb89cfad907c9e4b6
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-15 19:14:08 -0700

    WIP add date and description elements to views

--- BakeryOrders/Controllers/VendorsController.cs
+++ BakeryOrders/Controllers/VendorsController.cs
@@ -13,5 +13,49 @@ namespace BakeryOrders.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("Vendor", selectedVendor);
+      model.Add("Orders", vendorOrders);
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+
+      Vendor foundVendor = Vendor.Find(vendorId);
+
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice);
+
+      foundVendor.AddOrder(newOrder);
+
+      List<Order> vendorOrders = foundVendor.Orders;
+
+      model.Add("Orders", vendorOrders);
+
+      model.Add("Vendor", foundVendor);
+
+      return View("Show", model);
+    }
   }
 }
